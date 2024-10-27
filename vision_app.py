b25fc8b60ca366b14adcc8561df61b0326187fbc
paramjeet2003 <157730644+paramjeet2003@users.noreply.github.com>
2024-10-27 18:24:38 +0530

    Update vision_app.py

--- vision_app.py
+++ vision_app.py
@@ -4,8 +4,10 @@ import streamlit as st
 import google.generativeai as genai
 from PIL import Image
 load_dotenv()
-
-genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
+st.set_page_config(page_title="QnA Demo")
+st.header("Gemini LIM Application")
+api_key = st.sidebar.text_input("Enter the Google API key: ", type='password')
+genai.configure(api_key = api_key)
 ## Fuction to load the Gemini pro model and get responses
 model = genai.GenerativeModel("gemini-1.5-flash")
 def get_gemini_response(inputs,image):
@@ -15,20 +17,20 @@ def get_gemini_response(inputs,image):
         response = model.generate_content(image)
     return response.text
 
-st.set_page_config(page_title="QnA Demo")
-st.header("Gemini LIM Application")
+
 inputs = st.text_input("Input Prompt: ", key = "inputs")
 
-uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
-image = ""
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+if api_key:
+    uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
+    image = ""
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
 
-submit = st.button("Tell me about the image")
+    submit = st.button("Tell me about the image")
 
-if submit:
-    response = get_gemini_response(inputs, image)
-    st.subheader("The Response is: ")
-    st.write(response)
+    if submit:
+        response = get_gemini_response(inputs, image)
+        st.subheader("The Response is: ")
+        st.write(response)
     
